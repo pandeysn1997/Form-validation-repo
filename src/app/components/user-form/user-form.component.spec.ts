009a035edad6c2931d002f24bda2d837db8edd87
Sachin Pandey <s.pandey1@zensar.com>
2022-05-02 20:36:49 +0530

    this is commit

---
+++ src/app/components/user-form/user-form.component.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserFormComponent } from './user-form.component';
+
+describe('UserFormComponent', () => {
+  let component: UserFormComponent;
+  let fixture: ComponentFixture<UserFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('User form component', () => {
+    expect(component.idsList).toBeTruthy();
+});
+
+it('below data', () => {
+  expect(component.belowData).toBeDefined();
+});
+});
